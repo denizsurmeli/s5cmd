ceeae8da996dd4ab50fee24ed9424094f3250c5c
Deniz Sürmeli <denizsurmeli@icloud.com>
2023-07-11 20:12:42 +0300

    feat: partial implementation of thread safe ring buffer

---
+++ buffer/ring_buffer.go
@@ -0,0 +1,106 @@
+// A thread-safe ring buffer wrapper around the standard library.
+package buffer
+
+import (
+	"container/ring"
+	"errors"
+	"sync"
+)
+
+var (
+	taskNotCompleted  = errors.New("Task is not completed")
+	emptyWriteAttempt = errors.New("Empty write attempt to task")
+	bufferFull        = errors.New("Buffer is full")
+	notATask          = errors.New("node value is not a task")
+)
+
+// RingBuffer is a wrapper around the container/ring for
+// implementing thread-safe operations and
+// writeAt functionality.
+type RingBuffer struct {
+	r          *ring.Ring // next ptr to read
+	w          *ring.Ring // next ptr to write
+	start      *ring.Ring // lap ptr
+	wc         int        // number of writes to the buffer
+	rc         int        // number of reads from the buffer
+	lap        int        // number of laps in the ring
+	bufferSize int        // concurrent writers * (concurrent writers + 1) / 2
+	offsetSize int        // part size
+	chunks     int64      // expected total chunk count
+	mu         sync.Mutex
+
+	Items chan []byte
+}
+
+func (rb *RingBuffer) IsFull() bool {
+	rb.mu.Lock()
+	defer rb.mu.Unlock()
+
+	return rb.r == rb.w && rb.wc != 0
+}
+
+func (rb *RingBuffer) WriteAt(p []byte, off int64) (int, error) {
+	rb.mu.Lock()
+	defer rb.mu.Unlock()
+	if rb.wc > 0 && rb.wc%rb.bufferSize == 0 {
+		rb.lap++
+	}
+	if rb.r != rb.w || (rb.wc == 0 && rb.w == rb.r) || rb.wc-rb.rc <= rb.bufferSize {
+		index := (int(off) - (rb.lap * rb.bufferSize * rb.offsetSize)) / rb.offsetSize
+		var temp *ring.Ring
+		if index == 0 {
+			temp = rb.w
+		} else {
+			temp = rb.w
+			for index > 0 {
+				temp = temp.Next()
+				index--
+			}
+		}
+		temp.Value = p
+		rb.wc += 1
+		return rb.offsetSize, nil
+	}
+	return 0, bufferFull
+}
+
+func (rb *RingBuffer) Read() {
+	rb.mu.Lock()
+	defer rb.mu.Unlock()
+
+	if rb.r != rb.w || (rb.r.Value != nil && rb.rc == 0) || rb.wc-rb.rc <= rb.bufferSize {
+		defer func() {
+			// flush the item
+			rb.r.Value = nil
+			rb.r = rb.r.Next()
+		}()
+		if rb.r.Value != nil {
+			rb.rc += 1
+			rb.Items <- rb.r.Value.([]byte)
+		}
+	}
+
+	if rb.rc == int(rb.chunks) {
+		// you are done, close the channel
+		close(rb.Items)
+	}
+}
+
+// The guaranteed buffer size is (workerCount * (workerCount + 1) / 2)
+func NewRing(size int, offsetSize int, fileSize int64) *RingBuffer {
+	r := ring.New(size)
+	rb := &RingBuffer{
+		r:          r,
+		w:          r,
+		start:      r,
+		mu:         sync.Mutex{},
+		rc:         0,
+		wc:         0,
+		lap:        0,
+		bufferSize: size,
+		offsetSize: offsetSize,
+		chunks:     fileSize,
+		Items:      make(chan []byte, size)}
+
+	return rb
+}
---
+++ buffer/ring_buffer_test.go
@@ -0,0 +1,96 @@
+package buffer_test
+
+import (
+	"bytes"
+	"math"
+	"testing"
+
+	"github.com/peak/s5cmd/v2/buffer"
+)
+
+type WriteAtCase struct {
+	Order  []int
+	Offset int
+}
+
+func fillAndReturn(n, size int) []byte {
+	chunk := make([]byte, size)
+	for i := 0; i < size; i++ {
+		chunk[i] = byte(n)
+	}
+	return chunk
+}
+
+func calculateChunkCount(filesize, width int) int64 {
+	return int64(math.Ceil(float64(filesize / width)))
+}
+
+func TestWriteAtInOrderNoLap(t *testing.T) {
+	writerCount := 5
+	size := writerCount * (writerCount + 1) / 2
+	offset := 64
+
+	rb := buffer.NewRing(size, offset, int64(size))
+	for i := 1; i <= size; i++ {
+		chunk := fillAndReturn(i, offset)
+		_, err := rb.WriteAt(chunk, int64(offset*(i-1)))
+		if err != nil {
+			t.Errorf("Error while writing chunk\n")
+		}
+	}
+
+	for i := 1; i <= size; i++ {
+		rb.Read()
+		chunk := <-rb.Items
+		expected := fillAndReturn(i, offset)
+		if !bytes.Equal(chunk, expected) {
+			t.Errorf("Got %v, expected: %v\n", chunk, expected)
+		}
+	}
+}
+
+func TestWriteAtInOrderLap(t *testing.T) {
+	writerCount := 5
+	size := writerCount * (writerCount + 1) / 2
+	offset := 64
+	rb := buffer.NewRing(size, offset, int64(size))
+
+	// first round, lap 0
+	for i := 1; i <= size; i++ {
+		_, err := rb.WriteAt(fillAndReturn(i, offset), int64(offset*(i-1)))
+		if err != nil {
+			t.Errorf("Error while writing the chunk\n")
+		}
+	}
+	// drain the channel
+	for i := 1; i <= size; i++ {
+		rb.Read()
+		<-rb.Items
+	}
+
+	// reopen the channel, since you have closed by reading all the chunks
+	// this never happens when you concurrently both write and read,
+	// the purpose of this test to is the behaviour as expected
+	// when we lap in the buffer.
+	rb.Items = make(chan []byte, size)
+
+	// second round, lap 1
+	for i := 1; i <= size; i++ {
+		_, err := rb.WriteAt(fillAndReturn(i+size, offset), int64(offset*(i+size-1)))
+		if err != nil {
+			t.Errorf("Error while writing the chunk\n")
+		}
+
+	}
+
+	// read size items
+	for i := 1; i <= size; i++ {
+		rb.Read()
+		chunk := <-rb.Items
+		expected := fillAndReturn(size+i, offset)
+
+		if !bytes.Equal(chunk, expected) {
+			t.Errorf("Got %v, expected: %v\n", chunk, expected)
+		}
+	}
+}
